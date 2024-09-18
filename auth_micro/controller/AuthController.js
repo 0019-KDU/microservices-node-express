6557aec775601d08b28602e5ff52a2677240ea45
CR Hapuarachchi <chiranthahapuararchchi@gmail.com>
2024-09-18 23:26:28 +0530

    implement the auth controller with login and register functions with the token

--- auth_micro/controller/AuthController.js
+++ auth_micro/controller/AuthController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import prisma from "../config/db.config.js";
+import jwt from "jsonwebtoken";
 
 class AuthController {
   static async register(req, res) {
@@ -25,6 +26,48 @@ class AuthController {
         });
       }
       console.error("Registration Error: ", error);
+      return res.status(500).json({
+        status: "error",
+        message: "Something went wrong, please try again",
+      });
+    }
+  }
+
+  static async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await prisma.user.findUnique({ where: { email } });
+
+      if (user) {
+        //check both  password
+        if (!bcrypt.compareSync(password, user.password)) {
+          return res.status(401).json({
+            status: "error",
+            message: "Invalid credentials",
+          });
+        }
+        const payload = {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        return res.status(200).json({
+          status: "success",
+          message: "Logged in successfully",
+          data: { token },
+        });
+      }
+      return res.status(401).json({
+        status: "error",
+        message: "Invalid credentials",
+      });
+    } catch (error) {
+      console.error("Login Error: ", error);
       return res.status(500).json({
         status: "error",
         message: "Something went wrong, please try again",

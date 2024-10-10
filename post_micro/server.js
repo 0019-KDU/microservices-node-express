893c5b8b83ae8102b0c144216a38db78814efff6
CR Hapuarachchi <112224823+0019-KDU@users.noreply.github.com>
2024-10-10 20:55:02 +0530

    Post Microservice Setup

---
+++ post_micro/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+const app = express();
+
+const PORT = process.env.PORT || 5002;
+
+//* Middleware
+
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: false })); //
+
+// app.get("/", (req, res) => {
+//   return res.json({ message: "It's working" });
+// });
+
+// //* Routes
+// import Routes from "./routes/index.js";
+// app.use(Routes);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port ${PORT}`);
+});

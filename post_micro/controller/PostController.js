e4f6a81279678861645ddeb7ecadb31d6497d257
CR Hapuarachchi <112224823+0019-KDU@users.noreply.github.com>
2024-10-12 19:44:09 +0530

    improment
    optimize the data base queris
    We use the node-cache package to cache user data

--- post_micro/controller/PostController.js
+++ post_micro/controller/PostController.js
@@ -1,47 +1,54 @@
 import axios from "axios";
+import NodeCache from "node-cache";
 import prisma from "../config/db.config.js";
+
+// Create a cache instance
+const cache = new NodeCache({ stdTTL: 600, checkperiod: 120 });
 
 class PostController {
   static async index(req, res) {
     try {
+      // * Fetch posts
       const posts = await prisma.post.findMany({});
 
-      // * Method 2
-      let userIds = [];
-      posts.forEach((item) => {
-        userIds.push(item.user_id);
+      // * Extract user IDs from posts
+      const userIds = posts.map((item) => item.user_id);
+
+      // * Check cache for users
+      let users = cache.get("users");
+
+      if (!users) {
+        // * Fetch users from external microservice if not in cache
+        const response = await axios.post(
+          `${process.env.AUTH_MICRO_URL}/api/getUsers`,
+          userIds
+        );
+
+        users = {};
+        response.data.users.forEach((item) => {
+          users[item.id] = item;
+        });
+
+        // * Cache the user data for future use
+        cache.set("users", users);
+      }
+
+      // * Combine posts with their respective users
+      const postWithUsers = posts.map((post) => {
+        const user = users[post.user_id];
+        return {
+          ...post,
+          user,
+        };
       });
 
-      //   Fetch users
-      const response = await axios.post(
-        `${process.env.AUTH_MICRO_URL}/api/getUsers`,
-        userIds
-      );
-
-      const users = {};
-      response.data.users.forEach((item) => {
-        users[item.id] = item;
-      });
-
-      //   * Method 3
-      let postWithUsers = await Promise.all(
-        posts.map((post) => {
-          const user = users[post.user_id];
-
-          return {
-            ...post,
-            user,
-          };
-        })
-      );
-
+      // * Return the result
       return res.json({ postWithUsers });
     } catch (error) {
       console.log("the post fetch error is", error);
       return res.status(500).json({ message: "Something went wrong." });
     }
   }
-
   static async store(req, res) {
     try {
       const authUser = req.user; // Use req.user instead of req.authUser
